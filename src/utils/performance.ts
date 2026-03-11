9f4e05d82c2040ef647d61dd53b6d30020c91d3e
abdalrahman20091980-Alpa <abdalrahman20091980@gmail.com>
2026-03-11 17:49:01 +0200

    Delete src directory

--- src/utils/performance.ts
+++
@@ -1,7 +0,0 @@
-export const debounce = (fn: Function, delay: number) => {
-  let timer: NodeJS.Timeout;
-  return (...args: any[]) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => fn(...args), delay);
-  };
-};
